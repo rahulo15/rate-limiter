76083fa844a61defb68c38cee698c2e9500b3f28
Rahul Kumar <laddoo2016@gmail.com>
2026-02-19 10:55:41 +0530

    yaml files added

--- internal/middleware/rate_limit.go
+++ internal/middleware/rate_limit.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"rate-limiter/internal/limiter"
 
 	"github.com/redis/go-redis/v9"
@@ -15,13 +17,18 @@ func RateLimit(rdb *redis.Client) func(next http.Handler) http.Handler {
 				userID = r.RemoteAddr
 			}
 
-			if !limiter.AllowRequest(rdb, userID, 5, 5.0/60.0) {
+			podName, _ := os.Hostname()
+
+			allowed := limiter.AllowRequest(rdb, userID, 5, 5.0/60.0)
+
+			if allowed {
+				fmt.Printf("[%s] ✅ Allowed request for %s\n", podName, userID)
+				next.ServeHTTP(w, r)
+			} else {
+				fmt.Printf("[%s] 🚫 Rate Limited %s\n", podName, userID)
 				w.WriteHeader(http.StatusTooManyRequests)
 				w.Write([]byte(`{"error": "Too many requests"}`))
-				return
 			}
-
-			next.ServeHTTP(w, r)
 		})
 	}
 }
